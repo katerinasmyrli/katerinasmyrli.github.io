36df3c18a7f9098583e8dbc9012260d688f8130a
katerinasmyrli <92060995+katerinasmyrli@users.noreply.github.com>
2026-03-28 15:07:49 +0000

    Deploying to gh-pages from @ katerinasmyrli/katerinasmyrli.github.io@3a9daf32a05f181a198a33764e6126921b4577b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -428,9 +428,9 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-03-01-hrf/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-master-39-s-thesis",
+          title: 'Master&amp;#39;s thesis',
+          description: "Imperial College London, 2018",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
